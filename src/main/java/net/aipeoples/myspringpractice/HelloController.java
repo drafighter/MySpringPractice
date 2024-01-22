a58f6b4a265fe650f5243962695c66606d41d4f2
drafighter <ldap01@naver.com>
2024-01-22 20:00:28 +0900

    Initial commit

---
+++ src/main/java/net/aipeoples/myspringpractice/HelloController.java
@@ -0,0 +1,2 @@
+package net.aipeoples.myspringpractice;public class HelloController {
+}
---
+++ src/test/java/net/aipeoples/myspringpractice/MySpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aipeoples.myspringpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
