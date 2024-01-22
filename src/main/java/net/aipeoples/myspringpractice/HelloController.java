dc744af60cd4a24f2b7ff46f480dbfa0a66d092f
drafighter <ldap01@naver.com>
2024-01-22 21:23:26 +0900

    서블릿요청처리-파라미터 처리가 안되고 있음

--- src/main/java/net/aipeoples/myspringpractice/HelloController.java
+++ src/main/java/net/aipeoples/myspringpractice/HelloController.java
@@ -1,2 +1,14 @@
-package net.aipeoples.myspringpractice;public class HelloController {
+package net.aipeoples.myspringpractice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public  String hello(@PathVariable String name) {
+        return  "Hello " + name;
+    }
 }
--- src/main/java/net/aipeoples/myspringpractice/MySpringPracticeApplication.java
+++ src/main/java/net/aipeoples/myspringpractice/MySpringPracticeApplication.java
@@ -1,13 +1,35 @@
 package net.aipeoples.myspringpractice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class MySpringPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySpringPracticeApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                    String name = req.getParameter("name ");
+                    System.out.println(name);
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().print("Hello "+name);
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
     }
 
 }
